0b159bc83e016126120d5cf827d64757a0e4ff61
Mudabir Hussain <mudabirburfat97@gmail.com>
2024-09-13 15:43:49 +0400

    Initial commit

---
+++ src/main/java/security/jwt_oauth2server/JwOauth2serverApplication.java
@@ -0,0 +1,13 @@
+package security.jwt_oauth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwOauth2serverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwOauth2serverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/security/jwt_oauth2server/JwOauth2serverApplicationTests.java
@@ -0,0 +1,13 @@
+package security.jwt_oauth2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwOauth2serverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
