a349ace8325b6d05f3c5d0028b40f21c9b2b7e9b
Mudabir Hussain <mudabirburfat97@gmail.com>
2024-09-17 23:52:58 +0400

    JWT Implementation

--- src/main/java/security/jwt_oauth2server/JwOauth2serverApplication.java
+++ src/main/java/security/jwt_oauth2server/JwOauth2serverApplication.java
@@ -2,8 +2,11 @@ package security.jwt_oauth2server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import security.jwt_oauth2server.configs.RsaKeyProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class JwOauth2serverApplication {
 
 	public static void main(String[] args) {
